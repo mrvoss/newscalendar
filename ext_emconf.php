8a92fc5b62efca2935434fa74aea6f3ccaa7fe75
aquariuz <aquariuz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-02 00:31:25 +0000

    Version 2.1.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/newscalendar/trunk@29600 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "newscalendar".
+# Extension Manager/Repository config file for ext: "newscalendar"
 #
-# Auto generated 04-12-2009 03:02
+# Auto generated 02-02-2010 00:22
 #
 # Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Calendar for Ext:tt_news. Provides calendar and a list view functionality. Featured sponsors: geefgratis.nl, eventonizer.nl',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.1.1',
+	'version' => '2.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,9 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:46:{s:9:"ChangeLog";s:4:"af79";s:10:"README.txt";s:4:"7573";s:12:"ext_icon.gif";s:4:"f04e";s:17:"ext_localconf.php";s:4:"5708";s:14:"ext_tables.php";s:4:"77b3";s:14:"ext_tables.sql";s:4:"d348";s:15:"flexform_ds.xml";s:4:"7644";s:13:"locallang.xml";s:4:"2b85";s:16:"locallang_db.xml";s:4:"ed1b";s:14:"doc/manual.sxw";s:4:"354d";s:19:"doc/wizard_form.dat";s:4:"e21a";s:20:"doc/wizard_form.html";s:4:"5f71";s:49:"hooks/class.tx_newscalendar_additionalMarkers.php";s:4:"ab23";s:14:"pi1/ce_wiz.gif";s:4:"f04e";s:33:"pi1/class.tx_newscalendar_pi1.php";s:4:"79ec";s:41:"pi1/class.tx_newscalendar_pi1_wizicon.php";s:4:"1d8d";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"bf9b";s:24:"pi1/static/editorcfg.txt";s:4:"c760";s:14:"res/accept.png";s:4:"8bfe";s:29:"res/calendarViewTemplate.html";s:4:"23ab";s:14:"res/cancel.png";s:4:"757a";s:25:"res/class.newscalendar.js";s:4:"1c6b";s:19:"res/cssCalendar.css";s:4:"d15e";s:22:"res/cssContextMenu.css";s:4:"092e";s:19:"res/date_changer.js";s:4:"69b9";s:17:"res/jquery.min.js";s:4:"bb38";s:25:"res/listViewTemplate.html";s:4:"8cb1";s:30:"res/bt-0.9.5-rc1/CHANGELOG.txt";s:4:"86bf";s:29:"res/bt-0.9.5-rc1/jquery.bt.js";s:4:"f0ee";s:33:"res/bt-0.9.5-rc1/jquery.bt.min.js";s:4:"45ef";s:48:"res/bt-0.9.5-rc1/other_libs/jquery.easing.1.3.js";s:4:"6516";s:58:"res/bt-0.9.5-rc1/other_libs/jquery.hoverIntent.minified.js";s:4:"015b";s:56:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/ChangeLog.txt";s:4:"0a88";s:52:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/META.json";s:4:"bd9b";s:61:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.js";s:4:"880b";s:65:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.min.js";s:4:"a868";s:66:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.pack.js";s:4:"7b5a";s:47:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/AUTHORS";s:4:"e1b4";s:47:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/COPYING";s:4:"3b83";s:46:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/README";s:4:"b7e6";s:51:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/excanvas.js";s:4:"13b2";s:22:"res/pic/arrow-left.png";s:4:"9ccb";s:23:"res/pic/arrow-right.png";s:4:"6a8f";s:20:"static/constants.txt";s:4:"0280";s:16:"static/setup.txt";s:4:"f2b0";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:46:{s:9:"ChangeLog";s:4:"af79";s:10:"README.txt";s:4:"7573";s:12:"ext_icon.gif";s:4:"f04e";s:17:"ext_localconf.php";s:4:"5708";s:14:"ext_tables.php";s:4:"77b3";s:14:"ext_tables.sql";s:4:"d348";s:15:"flexform_ds.xml";s:4:"7644";s:13:"locallang.xml";s:4:"2b85";s:16:"locallang_db.xml";s:4:"ed1b";s:14:"pi1/ce_wiz.gif";s:4:"f04e";s:33:"pi1/class.tx_newscalendar_pi1.php";s:4:"bd7b";s:41:"pi1/class.tx_newscalendar_pi1_wizicon.php";s:4:"1d8d";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"bf9b";s:24:"pi1/static/editorcfg.txt";s:4:"c760";s:14:"doc/manual.sxw";s:4:"354d";s:19:"doc/wizard_form.dat";s:4:"e21a";s:20:"doc/wizard_form.html";s:4:"5f71";s:49:"hooks/class.tx_newscalendar_additionalMarkers.php";s:4:"ab23";s:14:"res/accept.png";s:4:"8bfe";s:29:"res/calendarViewTemplate.html";s:4:"23ab";s:14:"res/cancel.png";s:4:"757a";s:25:"res/class.newscalendar.js";s:4:"6a3c";s:19:"res/cssCalendar.css";s:4:"d15e";s:22:"res/cssContextMenu.css";s:4:"092e";s:19:"res/date_changer.js";s:4:"69b9";s:17:"res/jquery.min.js";s:4:"bb38";s:25:"res/listViewTemplate.html";s:4:"8cb1";s:22:"res/pic/arrow-left.png";s:4:"9ccb";s:23:"res/pic/arrow-right.png";s:4:"6a8f";s:30:"res/bt-0.9.5-rc1/CHANGELOG.txt";s:4:"86bf";s:29:"res/bt-0.9.5-rc1/jquery.bt.js";s:4:"f0ee";s:33:"res/bt-0.9.5-rc1/jquery.bt.min.js";s:4:"45ef";s:48:"res/bt-0.9.5-rc1/other_libs/jquery.easing.1.3.js";s:4:"6516";s:58:"res/bt-0.9.5-rc1/other_libs/jquery.hoverIntent.minified.js";s:4:"015b";s:56:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/ChangeLog.txt";s:4:"0a88";s:52:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/META.json";s:4:"bd9b";s:61:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.js";s:4:"880b";s:65:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.min.js";s:4:"a868";s:66:"res/bt-0.9.5-rc1/other_libs/bgiframe_2.1.1/jquery.bgiframe.pack.js";s:4:"7b5a";s:47:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/AUTHORS";s:4:"e1b4";s:47:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/COPYING";s:4:"3b83";s:46:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/README";s:4:"b7e6";s:51:"res/bt-0.9.5-rc1/other_libs/excanvas_r3/excanvas.js";s:4:"13b2";s:20:"static/constants.txt";s:4:"0280";s:16:"static/setup.txt";s:4:"f2b0";}',
 );
 
 ?>
